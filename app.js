14aa991ee07ad8870a2525c515b1205f3a98ea58
ThaiQuoc <quocb1906556@student.ctu.edu.vn>
2022-02-27 09:06:38 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const setupContactRoutes = require("./app/routes/contact.routes");
+//const {BadRequestError, errorHandler } = require("./app/errors");
+const cors = require("cors");
+const app = express();
+setupContactRoutes(app);
+// app.use((req, res, next) => {
+//     next(new BadRequestError(404, "Resource not found"));
+// });
+// app.use((err, req, res, next) => {
+//     errorHandler.handleError(err, res);
+// });
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) =>{
+    res.json({ message: "Welcome to contact book application."});
+});
+module.exports = app;
